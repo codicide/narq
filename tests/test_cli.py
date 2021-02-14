34e07f7c5fa5d32c3f65b9207a4687fbd9aea7bd
Tim Orme <tim@sironamedical.com>
2021-02-14 11:55:19 -0800

    Fix tests

--- tests/test_cli.py
+++ tests/test_cli.py
@@ -1,4 +1,5 @@
 import logging
+import logging.config
 import sys
 
 from click.testing import CliRunner
@@ -10,8 +11,19 @@ async def foobar(ctx):
     return 42
 
 
-def worker_start():
-    logging.basicConfig(stream=sys.stderr, level=logging.DEBUG)
+def worker_pre_init():
+    log_level = "DEBUG"
+    logging.config.dictConfig(
+        {
+            'version': 1,
+            'disable_existing_loggers': False,
+            'handlers': {
+                'narq.standard': {'level': log_level, 'class': 'logging.StreamHandler', 'formatter': 'narq.standard'}
+            },
+            'formatters': {'narq.standard': {'format': '%(asctime)s: %(message)s', 'datefmt': '%H:%M:%S'}},
+            'loggers': {'narq': {'handlers': ['narq.standard'], 'level': log_level}},
+        }
+    )
     return WorkerSettings(burst=True, functions=[foobar])
 
 
@@ -24,13 +36,13 @@ def test_help():
 
 def test_run():
     runner = CliRunner()
-    result = runner.invoke(cli, ['tests.test_cli:worker_start'])
+    result = runner.invoke(cli, ['tests.test_cli:worker_pre_init'])
     assert result.exit_code == 0
     assert 'Starting worker for 1 functions: foobar' in result.output
 
 
 def test_check():
     runner = CliRunner()
-    result = runner.invoke(cli, ['tests.test_cli:worker_start', '--check'])
+    result = runner.invoke(cli, ['tests.test_cli:worker_pre_init', '--check'])
     assert result.exit_code == 1
     assert 'Health check failed: no health check sentinel value found' in result.output
